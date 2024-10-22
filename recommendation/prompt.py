875c996044c3732d8d088159115900978697aec0
david <fabric_lee@naver.com>
2024-10-22 21:11:54 +0900

    prompt modified- distance

--- recommendation/prompt.py
+++ recommendation/prompt.py
@@ -24,18 +24,27 @@ if the user's question is about recommendation
         e. Do not get the '맛집' word in the results
     2. Check if the query is a response from the user to the previously generated Multi-turn conversation.
         * If the input is a short response or lacks specific details, such as just naming a menu item or a place they want to visit like '횟집', '흑돼지', it can be a reply to a question aimed at multi-turn conversations.
-    3. Analyze the question, gather the location, menuplace, and keyword information, then check if there are two or more elements. 
+        * [Important]However, in the case of Distance-based recommendations, if expressions like '근처', '기끼은', or '[location]에서 갈만한' are present, perform the task without a Multi-turn process.
+
+    3.  Call types based on context within recommendations
+        - Multi-turn
+        - Distance-based (Do not need to proceed Multi-turn essentially)
+        - Attribute-based
+        - Keyword-based
+
+    4. Analyze the question, gather the location, menuplace, and keyword information, then check if there are two or more elements. 
         * Based on this, go through the PROCESSING to determine the type Multi turn or Recommendation.
-    4. [select Multi-turn type]If there's less than two elements within [location, menuplace, keyword] for recommendation, just make the answer for getting enough information.
+    5. [select Multi-turn type]If there's less than two elements within [location, menuplace, keyword] for recommendation, just make the answer for getting enough information.
         - return 'Multi-turn'
         - If information about the menu or place is needed, generate a question asking the user what specific menu or place they are looking for.
         - If location information is required, ask the user where the desired location is.
-    5. [select Recommendation type] If there are two or more elements for recommendation based on the given information and existing data, select the most appropriate recommendation type from the options below that fits the question.
+    6. [select Recommendation type] If there are two or more elements for recommendation based on the given information and existing data, select the most appropriate recommendation type from the options below that fits the question.
         The type decision is depends on the original question that user asked.
+        - Distance-based: It's a distance-based model which doesn't need Multi-turn. Select this when the query includes distance-related keywords such as '근처', '가까운', '~에서 갈만한'
+            - For Distance-based, Okay to return recommendations in one go without multi-turn.
+            - However, it is also possible to provide an answer after a multi-turn conversation. In particular, if terms like 'nearby' or 'close' are present, a distance-based recommendation will be selected.
         - Attribute-based: It's an attribute-based aggregation model. When the query contains information that can be accurately aggregated, this model will be used to generate the query.
         - Keyword-based: It's a keyword-based condition model. If there's no aggregatable information in the query, but relevant keywords are present, this model will recommend similar items based on keyword similarity.
-        - Distance-based: It's a distance-based model. Select this when the query includes distance-related keywords such as 'nearby' or 'close.'
-            - For Distance-based, there should be 'retrieval_keyword' for searching.
           
 IMPORTANCE:
     * The response format should be like JSON. Only the results of the json format must be output.
@@ -95,9 +104,8 @@ EXAMPLES:
         Output:
         {{'response_type': 'Recommendation', 
         'recommendation_factors':{{'location':'애월', 'menu_place':['횟집'],'keyword': [], 'business_type': ['단품요리 전문', '가정식', '일식']}},
-        'processing': 'Since we have the information that the user wants "횟집" add this to the "menu_place". the menu's business_type can be commonly mapped as "단품요리 전문", "가정식", "일식".With the previous "location" "애월" already in "previous_gotten_info", there are two factors, so set the recommendation type. Although it's not a "keyword," the user mentioned "근처," so choose the distance-based recommendation type. And because "Distance-based" recommendation needs "retrieval_keyword", generate it as "애월 횟집".',
+        'processing': 'Since we have the information that the user wants "횟집" add this to the "menu_place". the menu's business_type can be commonly mapped as "단품요리 전문", "가정식", "일식".With the previous "location" "애월" already in "previous_gotten_info", And regardless of the number of elements, if there's a comment like "근처" it should be categorized as distance-based.
         'recommendation_type': 'Distance-based',
-        'retrieval_keyword': '애월 횟집'}}
 
         <example2>
         previous_gotten_info: 
